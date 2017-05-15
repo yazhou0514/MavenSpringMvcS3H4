b509d8a2c31c94c0fdd4674e65069abb2f3f4e79
yazho0514 <674275470@qq.com>
2017-05-15 09:33:58 +0800

    更新

--- src/main/java/com/phn/controller/UserController.java
+++ src/main/java/com/phn/controller/UserController.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.phn.controller;
 
 
@@ -8,8 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
